d933ead76574ded85ffdf077b76ccf02dfd46586
Mmesomaa <miraclenwaogu247@gmail.com>
2022-09-18 15:51:58 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number times
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		_putchar(");
+	}
+	_putchar('\n');
+}
+

f6f1f4c608f17e05479f3a25c43e8a9614a58003
Mmesomaa <miraclenwaogu247@gmail.com>
2022-09-19 14:50:52 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that updates value of a parameter it points to to 98
+ * reset_to_98 - funstion that updates value of a parameter it points to to 98
  * @n: pointer reset to 98
  * Return: nothing
  */
